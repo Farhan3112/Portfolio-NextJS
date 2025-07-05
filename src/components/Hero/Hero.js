2e2252255b60673ace49a8121ba232d125991dc9
Farhan Shamshad <126975284+Farhan3112@users.noreply.github.com>
2025-07-05 12:58:36 +0530

    Update Hero.js (resume)

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -14,7 +14,7 @@ const Hero = (props) => (
         <SectionText>
         I'm a final-year B.Tech IT student passionate about web development and problem-solving, currently building projects with the MERN stack.
         </SectionText>
-        <a href="https://drive.google.com/file/d/1zPimTQOHD_dezrNkf_3SeR7sQk1URnqV/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+        <a href="https://drive.google.com/file/d/199TodhRxcH3RgczZilhEXVIfVOE9-p--/view?usp=sharing" target="_blank" rel="noopener noreferrer">
           <Button>View Resume</Button>
         </a>
       </LeftSection>
